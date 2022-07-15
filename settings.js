c7a9fa952fa76e99283c6e45333229597bc2b6d8
yannngithub <97786441+yannngithub@users.noreply.github.com>
2022-07-15 21:19:57 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,41 @@
+/**
+   * Made By Fandyyy 🕴️
+   * Subscribe FBOTZ YT
+   * Follow https://instagram.com/_nzrlafndi
+   * Follow https://github.com/FBOTZ-YT
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Ubah Disini
+global.ownerNumber = ['62895401223315','6289506269853']
+global.ownerName = 'Yannn'
+global.packname = 'YaSya - Bot'
+global.author = '08971554717'
+global.prefa = ['!','.','#','/'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
+global.sessionName = 'sesi'
+
+//Message Nya Ubah Disini
+global.mess = {
+admin: 'You Are Not Admin!',
+botAdmin: 'Bot Not Admin!',
+botOwner: 'You Are Not My Owner!',
+group: 'Only Group!',
+private: 'Only Private Chat',
+wait: 'Loading...',
+done: 'Done!'
+}
+
+//Sesuaikan
+global.thumb = fs.readFileSync('./media/ichi.jpeg')
+global.donasi = fs.readFileSync('./media/donasi.jpg')
+global.thumbnail = fs.readFileSync('./media/ichi.mp4')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
